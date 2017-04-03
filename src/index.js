17fba7351e4ab4ca061ef0e35a5da94e58e9a351
jonpowers <jon.powers@icloud.com>
2017-04-02 22:29:49 -0400

    More squirrel code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
+var app = require('app');
+var path = require('path');
+var cp = require('child_process');
+
+var handleSquirrelEvent = function() {
+   if (process.platform != 'win32') {
+      return false;
+
+   }
+
+   function executeSquirrelCommand(args, done) {
+      var updateDotExe = path.resolve(path.dirname(process.execPath), 
+         '..', 'update.exe');
+      var child = cp.spawn(updateDotExe, args, { detached: true });
+
+      child.on('close', function(code) {
+         done();
+      });
+   };
+
+   function install(done) {
+      var target = path.basename(process.execPath);
+      executeSquirrelCommand(["--createShortcut", target], done);
+   };
+
+   function uninstall(done) {
+      var target = path.basename(process.execPath);
+      executeSquirrelCommand(["--removeShortcut", target], done);
+   };
+
+   var squirrelEvent = process.argv[1];
+
+   switch (squirrelEvent) {
+
+      case '--squirrel-install':
+         install(app.quit);
+         return true;
+
+      case '--squirrel-updated':
+         install(app.quit);
+         return true;
+
+      case '--squirrel-obsolete':
+         app.quit();
+         return true;
+
+      case '--squirrel-uninstall':
+         uninstall(app.quit);
+         return true;
+   }
+
+   return false;
+};
+
+if (handleSquirrelEvent()) {
+   return;
+}
+
 import { app, globalShortcut, BrowserWindow, Tray } from 'electron';
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 import { enableLiveReload } from 'electron-compile';
